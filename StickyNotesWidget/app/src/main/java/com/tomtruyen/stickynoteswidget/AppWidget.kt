c253d4b8fd60c06fd2b9fb3b7b580b1dc257d33a
TomTruyen <tom.truyen@gmail.com>
2021-07-30 20:46:23 +0200

    Fix sticky notes click not working after a bit

--- StickyNotesWidget/app/src/main/java/com/tomtruyen/stickynoteswidget/AppWidget.kt
+++ StickyNotesWidget/app/src/main/java/com/tomtruyen/stickynoteswidget/AppWidget.kt
@@ -1,5 +1,6 @@
 package com.tomtruyen.stickynoteswidget
 
+import android.annotation.SuppressLint
 import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
@@ -8,15 +9,19 @@ import android.content.Intent
 import android.widget.RemoteViews
 
 class AppWidget : AppWidgetProvider() {
+    @SuppressLint("UnspecifiedImmutableFlag")
     override fun onUpdate(
         context: Context?,
         appWidgetManager: AppWidgetManager?,
         appWidgetIds: IntArray?
     ) {
         val launchIntent = Intent(context, MainActivity::class.java)
-        val pendingIntent = PendingIntent.getActivity(context, 0, launchIntent, 0)
+        launchIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK;
+
+        val pendingIntent = PendingIntent.getActivity(context, 0, launchIntent, PendingIntent.FLAG_UPDATE_CURRENT)
 
         val remoteViews = RemoteViews(context?.packageName, R.layout.widget_layout)
+
         remoteViews.setOnClickPendingIntent(R.id.widgetLayout, pendingIntent)
 
         appWidgetManager?.updateAppWidget(appWidgetIds, remoteViews)
