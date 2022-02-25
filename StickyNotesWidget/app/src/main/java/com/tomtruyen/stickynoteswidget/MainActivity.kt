c1f388136baf57032b2cea5f755d04f9bc4fea1a
TomTruyen <tom.truyen@gmail.com>
2022-02-25 13:34:09 +0100

    Add banner ads
    Monetization

--- StickyNotesWidget/app/src/main/java/com/tomtruyen/stickynoteswidget/MainActivity.kt
+++ StickyNotesWidget/app/src/main/java/com/tomtruyen/stickynoteswidget/MainActivity.kt
@@ -6,15 +6,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.RemoteViews
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
 import com.tomtruyen.stickynoteswidget.models.StickyNote
 
 class MainActivity : AppCompatActivity() {
     private lateinit var note : StickyNote
     private lateinit var editText: EditText
+    private lateinit var mAdView: AdView;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        MobileAds.initialize(this) {}
+
+        mAdView = findViewById(R.id.adView)
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
 
         editText = findViewById(R.id.editText)
 
