cd256aeacb0cb176c12f5389d01ae39c1ae6c115
Tom Truyen <tom.truyen@gmail.com>
2023-08-29 20:00:12 +0200

    refactor: add binding to reduce crashes

--- StickyNotesWidget/app/src/main/java/com/tomtruyen/stickynoteswidget/MainActivity.kt
+++ StickyNotesWidget/app/src/main/java/com/tomtruyen/stickynoteswidget/MainActivity.kt
@@ -9,44 +9,47 @@ import android.widget.RemoteViews
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdView
 import com.google.android.gms.ads.MobileAds
+import com.tomtruyen.stickynoteswidget.databinding.ActivityMainBinding
 import com.tomtruyen.stickynoteswidget.models.StickyNote
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var note : StickyNote
-    private lateinit var editText: EditText
-    private lateinit var mAdView: AdView;
+    private lateinit var mBinding: ActivityMainBinding
+
+    private val note = StickyNote(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        MobileAds.initialize(this) {}
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
 
-        mAdView = findViewById(R.id.adView)
-        val adRequest = AdRequest.Builder().build()
-        mAdView.loadAd(adRequest)
+        setupAds()
 
-        editText = findViewById(R.id.editText)
-
-        note = StickyNote(this)
         val text = note.getSticky()
         if(text != null && text != "") {
-            editText.setText(text)
+            mBinding.editText.setText(text)
         }
     }
 
     override fun onStop() {
-        note.setSticky(editText.text.toString())
+        note.setSticky(mBinding.editText.text.toString())
         updateWidget()
 
         super.onStop()
+    }
+
+    private fun setupAds() {
+        MobileAds.initialize(this) {}
+
+        val request = AdRequest.Builder().build()
+        mBinding.adView.loadAd(request)
     }
 
     private fun updateWidget() {
         val appWidgetManager = AppWidgetManager.getInstance(this)
         val remoteViews = RemoteViews(this.packageName, R.layout.widget_layout)
         val widget = ComponentName(this, AppWidget::class.java)
-        remoteViews.setTextViewText(R.id.widgetText, editText.text.toString())
+        remoteViews.setTextViewText(R.id.widgetText, mBinding.editText.text.toString())
         appWidgetManager.updateAppWidget(widget, remoteViews)
     }
 }
